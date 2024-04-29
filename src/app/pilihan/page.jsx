46d275f5dc72806940c05bace2dd2bad69549665
Natanael Rio Wijaya <gafterlife@gmail.com>
2024-04-29 22:15:59 +0700

    v3

--- src/app/pilihan/page.jsx
+++ src/app/pilihan/page.jsx
@@ -1,8 +1,12 @@
 import LayoutHeaderFooter from '@/component/layoutHeaderFooter';
 import PilihanUser from '@/component/pilihanUser';
+export const metadata = {
+    title: 'ORGAN PERNAFASAN MANUSIA',
+    description: 'ORGAN PERNAFASAN MANUSIA',
+}
+
 export default function Pilihan() {
     return (
-
         <>
             <LayoutHeaderFooter kondisi={true}>
                 <PilihanUser />
--- src/app/rumus/page.jsx
+++ src/app/rumus/page.jsx
@@ -1,5 +1,10 @@
 import HalamanRumus from '@/component/halamanRumus'
 
+export const metadata = {
+  title: 'ORGAN PERNAFASAN MANUSIA',
+  description: 'ORGAN PERNAFASAN MANUSIA',
+}
+
 export default function Rumus() {
   return (
     <HalamanRumus />
