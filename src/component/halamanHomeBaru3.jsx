7fcc1d83fa53ec73695365990e21b64eea3c37fe
Natanael Rio Wijaya <gafterlife@gmail.com>
2024-06-20 16:04:27 +0700

    v16

--- src/component/halamanHomeBaru3.jsx
+++ src/component/halamanHomeBaru3.jsx
@@ -1,13 +1,11 @@
 import styles from '@/component/halamanHomeBaru3.module.css'
-import Image from 'next/image'
-import { FaArrowTurnDown } from "react-icons/fa6";
 import LayoutHalamanAtas from './layoutHalamanAtas';
 
 export default function HalamanHomeBaru3() {
   return (
     <>
       <LayoutHalamanAtas gambar={'logoutamabaru.png'} katabawah={'Geser Kebawah'}>
-        <div className={styles.judul}>SISTEM PENAPASAN MANUSIA</div>
+        <div className={styles.judul}>SISTEM PERNAPASAN MANUSIA</div>
         <div className={styles.subjudul}>
           Hai anak-anak, Pada website ini kita akan belajar tentang organ sistem pernapasan dan alur pernapasan pada manusia.
         </div>
